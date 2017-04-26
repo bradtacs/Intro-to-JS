b1ee306b3865c65f41becda2eaba3407a1d1a9be
Zachary Viggiani <zach@bigmojo.net>
2017-04-26 12:25:47 -0400

    Strings Done

--- strings.js
+++ strings.js
@@ -1,10 +1,5 @@
-var numThatLooksLikeAnInteger = 1;
-var numThatLooksLikeAFloat = 1;
-typeof(numThatLooksLikeAnInteger);
-"number"
-typeof(numThatLooksLikeAFloat);
-"number"
-numThatLooksLikeAnInteger
-1
-numThatLooksLikeAFloat
-1
+var num_ghosts = 4;
+var name = 'Pac-Man';
+var sentence = 'Our hero, ' + name + ', must eat ' + num_ghosts + ' ghosts for maximum points.';
+sentence
+'Our hero, Pac-Man, must eat 4 ghosts for maximum points.'
