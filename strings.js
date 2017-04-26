2b4dcbf47807a1ae24f9741a67400f0c2e329c91
Zachary Viggiani <zach@bigmojo.net>
2017-04-25 22:08:32 -0400

    strings

---
+++ strings.js
@@ -0,0 +1,10 @@
+var numThatLooksLikeAnInteger = 1;
+var numThatLooksLikeAFloat = 1;
+typeof(numThatLooksLikeAnInteger);
+"number"
+typeof(numThatLooksLikeAFloat);
+"number"
+numThatLooksLikeAnInteger
+1
+numThatLooksLikeAFloat
+1
